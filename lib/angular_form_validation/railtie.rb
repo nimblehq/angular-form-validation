9febd61d254bffc41a12765326caa6b14b08ee7d
Olivier Robert <olivierobert@gmail.com>
2014-10-15 12:09:37 +0700

    Set default form builder

--- lib/angular_form_validation/railtie.rb
+++ lib/angular_form_validation/railtie.rb
@@ -3,6 +3,7 @@ require 'rails'
 module AngularFormValidation
   class Railtie < ::Rails::Railtie
     initializer "angular_form_validation.setup_view_helpers" do
+      ActionView::Base.default_form_builder = ::AngularFormValidation::FormBuilder
       ActiveSupport.on_load(:action_view) do
         include ::AngularFormValidation::Helpers
       end
